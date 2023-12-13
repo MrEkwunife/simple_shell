8a870e5abe181bd8e972d37ce4942c196f536feb
Kenechukwu Akubue <kengoon19@gmail.com>
2023-12-13 21:45:42 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	printf("$\n");
+	return (0);
+}
+
